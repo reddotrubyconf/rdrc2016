85dad5cabf9315cec0e45b0ba25979ab16ac500a
Guo Xiang Tan <tgx_world@hotmail.com>
2016-05-10 21:02:20 +0800

    Add anchor for speakers.

--- db/schema.rb
+++ db/schema.rb
@@ -15,5 +15,6 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
 end
