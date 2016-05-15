e69914d22ffc2d656b2276ca29961f3991d2a2db
Winston <winston.yongwei@gmail.com>
2016-05-15 22:30:18 +0800

    Add accommodation-partners page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get "/code-of-conduct", to: "home#code_of_conduct"
+
+  get "/code-of-conduct",         to: "home#code_of_conduct"
+  get "/accommodation-partners",  to: "home#accommodation_partners"
+
   get "/cfp", to: redirect("https://rdrc-cfp.herokuapp.com/events/reddotrubyconf")
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
