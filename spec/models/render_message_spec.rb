0dda9b6fdc4cc090ffbd381065d163c087044dbe
JuanitoFatas <katehuang0320@gmail.com>
2016-06-22 16:51:16 +0800

    Fix build

--- spec/models/render_message_spec.rb
+++ spec/models/render_message_spec.rb
@@ -6,13 +6,10 @@ RSpec.describe RenderMessage do
       RenderMessage.new(nickname, created_at, content, announcement: announcement).call
     end
 
-    before { travel_to Time.new(2016, 1, 1, 0, 0, 0) }
-    after { travel_back }
-
     it "renders correct HTML" do
-      result = render("realDonaldTrump", Time.current, "Make America Great Again")
+      result = render("realDonaldTrump", "2016-01-01T00:00:00Z", "Make America Great Again")
 
-      expect(result).to eq %(<div class=\"message-body\"><strong>realDonaldTrump </strong><span class=\"text-muted\"><time datetime=\"2015-12-31T16:00:00Z\" data-local=\"time-ago\">December 31, 2015  4:00pm</time></span><p class=\"message-body \">Make America Great Again</p></div>)
+      expect(result).to eq %(<div class=\"message-body\"><strong>realDonaldTrump </strong><span class=\"text-muted\"><time datetime=\"2016-01-01T00:00:00Z\" data-local=\"time-ago\">January  1, 2016 12:00am</time></span><p class=\"message-body \">Make America Great Again</p></div>)
     end
 
     context "announcement" do
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -28,15 +28,12 @@ RSpec.describe "Messages" do
   end
 
   describe "#render_message" do
-    before { travel_to Time.new(2016, 1, 1, 0, 0, 0) }
-    after { travel_back }
-
     it "renders a message html" do
       sign_in create(:user)
 
-      post render_message_path, params: { nickname: "realDonaldTrump" , timestamp: Time.current, message: "Make America Great Again" }
+      post render_message_path, params: { nickname: "realDonaldTrump" , timestamp: "2016-01-01T00:00:00", message: "Make America Great Again" }
 
-      expect(response.body).to eq %(<div class=\"message-body\"><strong>realDonaldTrump </strong><span class=\"text-muted\"><time datetime=\"2015-12-31T16:00:00Z\" data-local=\"time-ago\">December 31, 2015  4:00pm</time></span><p class=\"message-body \">Make America Great Again</p></div>)
+      expect(response.body).to eq %(<div class=\"message-body\"><strong>realDonaldTrump </strong><span class=\"text-muted\"><time datetime=\"2016-01-01T00:00:00Z\" data-local=\"time-ago\">January  1, 2016 12:00am</time></span><p class=\"message-body \">Make America Great Again</p></div>)
     end
   end
 end
